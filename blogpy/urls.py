90af3eae189b089462108a2bbead87e78bc1fe13
ryan-abolfazl <ryan.abolfazl@gmail.com>
2024-11-10 11:51:31 +0330

    Update urls.py

--- blogpy/urls.py
+++ blogpy/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
     path('api-auth/', include('rest_framework.urls'))
-#     ]
+    ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
